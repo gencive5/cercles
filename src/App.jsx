0b051a901b40d0fa47d442196d9d0b75198d4b95
gencive5 <vic.segen@gmail.com>
2025-09-16 17:37:52 +0200

    test fix mobile height

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,6 @@ import CircleGrid from './CircleGrid';
 import Contact from './Contact';
 
 
-const resizeOps = () => {
-    document.documentElement.style.setProperty("--vh", window.innerHeight * 0.01 + "px");
-  };
-
-  resizeOps();
-  window.addEventListener("resize", resizeOps);
-
-
-
 function App() {
   return (
     
