da938ad50e90315b16661e00507d04165a94bc47
gencive5 <vic.segen@gmail.com>
2025-09-16 17:24:01 +0200

    test fix mobile
    android :/

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,15 @@ import CircleGrid from './CircleGrid';
 import Contact from './Contact';
 
 
+const resizeOps = () => {
+    document.documentElement.style.setProperty("--vh", window.innerHeight * 0.01 + "px");
+  };
+
+  resizeOps();
+  window.addEventListener("resize", resizeOps);
+
+
+
 function App() {
   return (
     
